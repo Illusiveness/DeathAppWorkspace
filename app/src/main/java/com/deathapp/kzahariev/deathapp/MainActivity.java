65e1ad7ba38d569b02c4ca5aa74344e088a6100a
k.zahariev <krumaki@gmail.com>
2018-07-11 14:58:30 +0300

    Version 0.1

---
+++ app/src/main/java/com/deathapp/kzahariev/deathapp/MainActivity.java
@@ -0,0 +1,51 @@
+package com.deathapp.kzahariev.deathapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private CallbackManager FBCallBackManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+        setContentView(R.layout.activity_main);
+        FBCallBackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(FBCallBackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+
+    }
+
+}
+
+
